be52ecddd9abfdc8a0e98a0959bfa80b36686dc9
Chris Emerson <github@mail.nosreme.org>
2020-11-03 20:30:50 +0000

    Add argument parsing, and and options for output file and renaming.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use gpx::Gpx;
-use std::fs::File;
-use std::io::BufReader;
+use structopt::StructOpt;
+
+#[derive(Debug,StructOpt)]
+#[structopt(name = "gpxmerge", about = "GPX merging tool")]
+pub struct Opts {
+    #[structopt(short = "R", long = "rename")]
+    rename: Option<String>,
+
+    #[structopt(short = "o", long = "output")]
+    output: String,
+
+    files: Vec<String>,
+}
 
 fn open_gpx(s: &str) -> Gpx {
     let f = std::fs::File::open(s).unwrap();
@@ -9,16 +20,30 @@ fn open_gpx(s: &str) -> Gpx {
 }
 
 fn main() {
-    let mut filenames = std::env::args().skip(1);
-    let mut gpx = open_gpx(&filenames.next().unwrap());
+    let opt = Opts::from_args();
+    let mut gpx = open_gpx(&opt.files[0]);
     println!("Opened a gpx");
     assert!(gpx.tracks.len() == 1);
 
-    for filename in filenames {
+    if let Some(new_name) = opt.rename.as_ref() {
+        match gpx.metadata {
+            None => {
+                gpx.metadata = Some(gpx::Metadata {
+                    name: Some(new_name.into()),
+                    ..Default::default()
+                });
+            }
+            Some(ref mut metadata) => {
+                metadata.name = Some(new_name.into());
+            }
+        }
+    }
+
+    for filename in &opt.files[1..] {
         let new_gpx = open_gpx(&filename);
         for track in new_gpx.tracks {
             gpx.tracks[0].segments.extend(track.segments);
         }
     }
-    gpx::write(&gpx, std::fs::File::create("test.gpx").unwrap()).unwrap();
+    gpx::write(&gpx, std::fs::File::create(&opt.output).unwrap()).unwrap();
 }
